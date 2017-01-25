14cb435a56c5cd5d733ab2d5a9ad52e2a33aa71c
Fábio Luciano <fabio@naoimporta.com>
2017-01-25 18:35:37 -0200

    hum

--- common/util.js
+++ common/util.js
@@ -21,7 +21,12 @@ module.exports = (() => {
     const things = filesystem.readdirSync(fatherDirectory);
 
 
-    console.log(recursive(fatherDirectory, ['template']));
+    var taskFiles = recursive(fatherDirectory, ['template']);
+
+    for(var file of taskFiles) {
+      var regex = /\/task\/((.*?)[\/.])+/g;
+      console.log(file.match(regex))
+    }
     // for(thing of things) {
     //   const thingPath = fatherDirectory + thing,
     //     thingStat = filesystem.statSync(thingPath);
